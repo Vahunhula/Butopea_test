2a3e0eb5e52941b31a7e7212659e470942be9119
Vahunhula <Vaxosheylashvili21@gmail.com>
2026-04-29 18:11:19 +0200

    Tutorial ch.9: computed fields, onchange, inverse function

--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,3 +15,21 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(base_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - base_date).days
